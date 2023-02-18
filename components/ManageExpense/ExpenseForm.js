656f5c444c5ec8c8e8ac605f18cd1b0ff74a808a
harsh9539 <goyalh862@gmail.com>
2023-02-19 00:00:16 +0530

    adding styling to form

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -4,28 +4,51 @@ import Input from './Input'
 
 export default function ExpenseForm() {
 
-    function amountChangeHandler(){
+    function amountChangeHandler() {
 
     }
     return (
-        <View>
+        <View style={styles.form}>
+            <Text style={styles.title}>Your Expense</Text>
+            <View style={styles.inputContainer}>
+                <Input
+                    label={"Amount"}
+                    keyboardType="number-pad"
+                    onChangeText={amountChangeHandler}
+                    style={styles.rowInput}
+                />
+                <Input
+                    label={"Date"}
+                    placeholder="YYYY-MM-DD"
+                    maxLength={10}
+                    onChangeText={() => { }}
+                    style={styles.rowInput}
+                />
+            </View>
             <Input
-            label={"Amount"}
-            keyboardType="number-pad"
-            onChangeText={amountChangeHandler}
-            />
-            <Input
-            label={"Date"}
-            placeholder="YYYY-MM-DD"
-            maxLength={10}
-            onChangeText={()=>{}}
-            />
-            <Input
-            label={"Description"}  
-            multiline={true}
+                label={"Description"}
+                multiline={true}
             />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    form:{
+        marginTop:40,
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        color:'white',
+        textAlign:'center',
+        marginVertical:20
+    },
+    inputContainer:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    rowInput:{
+        flex:1
+    }
+})
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,13 +1,40 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { GlobalStyles } from '../../constants/styles'
 
-export default function Input({label,...textInput}) {
+export default function Input({label,style,...textInput}) {
+    let inputStyles = [styles.input];
+
+    if(textInput&&textInput.multiline){
+        inputStyles.push(styles.inputMultiline);
+    }
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInput}/>
+        <View style={[styles.inputContainer,style]}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={inputStyles} {...textInput}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    inputContainer:{
+        marginHorizontal:4,
+        marginVertical:8,
+    },
+    label:{
+        fontSize:12,
+        color:GlobalStyles.colors.primary100,
+        marginBottom:4
+    },
+    input:{
+        backgroundColor:GlobalStyles.colors.primary100,
+        padding:6,
+        borderRadius:6,
+        fontSize:18,
+        color:GlobalStyles.colors.primary700
+    },
+    inputMultiline:{
+        minHeight:100,
+        textAlignVertical:'top'
+    }
+})
